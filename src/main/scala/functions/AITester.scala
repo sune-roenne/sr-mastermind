75ebb75ffc06efb2219a43e5e77f683afa76e45f
Sune <Sune@DESKTOP-IPQ33IN>
2017-12-29 10:28:18 +0100

    Worked on test execution

--- src/main/scala/functions/AITester.scala
+++ src/main/scala/functions/AITester.scala
@@ -20,15 +20,15 @@ object AITester {
     (for(i <- 0 until rounds) yield {
       val correctAnswer = generateRandomRow(listSize)
       var possibles = GameFunctions.generateAllPossibles(PieceColor.commonColors, listSize)
-      var guesses : List[PiecesRow] = Nil
-      var timings : List[Long] = Nil
+      var guesses = scala.collection.mutable.ArrayBuffer.empty[PiecesRow] 
+      var timings = scala.collection.mutable.ArrayBuffer.empty[Long]
       while(possibles.size > 1) {
         Try{
           val startTime = System.currentTimeMillis
-          val nextGuess = ai.nextGuess(possibles, guesses)
+          val nextGuess = ai.nextGuess(possibles, guesses.toList)
           val timeSpent = System.currentTimeMillis - startTime
-          guesses = (nextGuess :: guesses.reverse).reverse
-          timings = (timeSpent :: timings.reverse).reverse
+          guesses += nextGuess
+          timings += timeSpent
           val (corr, corrCol) = GameFunctions.likeness(correctAnswer, nextGuess)
           possibles = GameFunctions.trimPossibles(possibles, nextGuess, corr, corrCol)
         } match {
@@ -36,7 +36,7 @@ object AITester {
           case _ => 
         }
       }
-      TestRoundResult(correctAnswer, guesses, timings)
+      TestRoundResult(correctAnswer, guesses.toList, timings.toList)
       
       
     }).toList
--- src/test/scala/explorations/AITesterExplorations.scala
+++ src/test/scala/explorations/AITesterExplorations.scala
@@ -2,11 +2,33 @@ package explorations
 
 import functions.AITester
 import functions.ByWorstCaseFeedbackAI
+import java.io.File
+import java.io.PrintWriter
 
 object AITesterExplorations {
+  val outputFolder = new File("""c:\temp\mastermind""")
+  if(!outputFolder.exists)
+    outputFolder.mkdirs()
+  
   def main(args: Array[String]): Unit = {
-    val testResult = AITester.executeTest(ByWorstCaseFeedbackAI, 1, 4)
+    val underTest = ByWorstCaseFeedbackAI
+    val underTestName = underTest.getClass.getName.replaceAll("(\\$)|(functions\\.)", "")
+    println(underTestName)
+    val testResult = AITester.executeTest(ByWorstCaseFeedbackAI, 2, 4)
     println(testResult)
+    val outputFile = new File(outputFolder, "exectest_" + underTestName + "_" + System.currentTimeMillis()  + ".csv")
+    outputFile.createNewFile()
+    val writer = new PrintWriter(outputFile)
+    writer.write("Run;Answer;CorrectAnswerFound;NoOfGuesses;TotalTime\r\n")
+    writer.write(
+       testResult.zipWithIndex.map({ case (res,indx) => (indx + 1) + ";" +  res.correctAnswer + ";" + res.guesses.endsWith(List(res.correctAnswer)) + ";" + res.guesses.size + ";" + (res.millisecondsUsed.sum/1000L) }).mkString("\r\n")
+    )
+    
+    writer.close()
+    
+    
     
   }
+  
+  
 }
